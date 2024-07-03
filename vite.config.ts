0bf090d5ba5e444c883183b1a3209f2539b62627
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2024-07-03 14:21:55 +0530

    configuration to use absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path"
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    }
+  },
   plugins: [react()],
 })
